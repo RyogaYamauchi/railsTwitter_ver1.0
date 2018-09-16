536da84f3ec81af09ec5a53673ff1997c9317edd
Ryoga_Yamauchi <ya.ryouga.ma@docomo.ne.jp>
2018-09-17 00:09:28 +0900

    update

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,10 @@
 class CalendarController < ApplicationController
+  require 'google/apis/calendar_v3'
+  require 'googleauth'
+  require 'googleauth/stores/file_token_store'
+  require 'fileutils'
 
-  OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
+  OOB_URI = "http://localhost:3000/callbacked".freeze
   APPLICATION_NAME = 'Google-Calendar-to-Twitter'.freeze
   CREDENTIALS_PATH = 'credentials.json'.freeze
   TOKEN_PATH = 'client_secret.json'.freeze
@@ -12,19 +16,21 @@ class CalendarController < ApplicationController
     require 'googleauth'
     require 'googleauth/stores/file_token_store'
     require 'fileutils'
-
-    client_id = "240402029248-dfg771av1ehtn1fht7hecpj5lqjkntb1.apps.googleusercontent.com"
+    client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
     token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
-    authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-    user_id = 'primary'
+    authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store,"http://localhost:3000/callbacked")
+    user_id = 'watanuki.pazudora@gmail.com'
     credentials = authorizer.get_credentials(user_id)
     if credentials.nil?
       code = params[:code]
-      user_id = 'primary'
       credentials = authorizer.get_and_store_credentials_from_code(
         user_id: user_id, code: code, base_url: OOB_URI
       )
     end
     credentials
+  end
+
+  def callbacked
+
   end
 end
